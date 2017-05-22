69346568e1a43b90629fa6ef3a25dcc61b81c9a8
zhangjingqiang <zhangjingqiang@gmail.com>
2017-05-22 22:10:22 +0900

    Add destroy dependent

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :sites, dependent: :destroy
+
   validates_presence_of :name
 
   def to_param
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,5 @@
 class Site < ActiveRecord::Base
+  belongs_to :category
+
   validates_presence_of :name, :link, :image
 end
